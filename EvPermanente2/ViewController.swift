24b46f8a2492206e62238303d6224c7578a89d3e
Axel <salvatierraaxel123@gmail.com>
2020-10-07 00:42:50 -0500

    segundo commit

--- EvPermanente2/ViewController.swift
+++ EvPermanente2/ViewController.swift
@@ -9,9 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
+    
+    
+    @IBAction func clickClosekeyboard(_ sender: Any) {
+        print("CERRAR TECLADO ACCION")
+        self.view.endEditing(true)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification){
+        
+        print("EL TECLADO APARECE")
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        print(keyboardFrame)
+        print(animationDuration)
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+        
+        print("EL TECLADO SE OCULTA")
+        
     }
 
 
