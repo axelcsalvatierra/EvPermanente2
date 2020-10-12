4fb9309229d67c0d0857ec990f77bf207103cece
Axel <salvatierraaxel123@gmail.com>
2020-10-11 21:22:28 -0500

    8vo commit

---
+++ EvPermanente2/RestablecerContrasenaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RestablecerContrasenaViewController.swift
+//  EvPermanente2
+//
+//  Created by Axel Salvatierra on 10/11/20.
+//
+
+import UIKit
+
+class RestablecerContrasenaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
