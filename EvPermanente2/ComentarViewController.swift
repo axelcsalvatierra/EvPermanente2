c4b05860f47e3b5c4150ab3827e63c7be188fae4
Axel Salvatierra <axelsc@Mac-de-Axel.local>
2020-12-03 22:44:26 -0500

    EvPermanente4 3er commit

--- EvPermanente2/ComentarViewController.swift
+++ EvPermanente2/ComentarViewController.swift
@@ -81,5 +81,65 @@ class CommentDataModel{
     static let shareInstance = CommentDataModel()
     var arrComments = [CommentModel]()
 }*/
+    
+    
+    
+    
+ //ANIMACION DE PANTALLA Y TECLADO
+    
+    
+    /*@IBOutlet weak var ConstraintCenterYContent: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var viewContent: UIView!
+    @IBAction func clickClosekeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification){
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        let finalPosyContent = self.viewContent.frame.origin.y + self.viewContent.frame.height
+        let originkeyboardY = keyboardFrame.origin.y
+        var delta: CGFloat = 0
+        
+        if originkeyboardY < finalPosyContent {
+            delta = originkeyboardY - finalPosyContent - 75
+        }
 
+        UIView.animate(withDuration: animationDuration) {
+            
+            self.constraintCenterYContent.constant = delta
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            
+            self.constraintCenterYContent.constant = 0
+            self.view.layoutIfNeeded()
+        }
+        
+    }*/
+    
 }
---
+++ EvPermanente2/HacerPublicaciónViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HacerPublicaciónViewController.swift
+//  EvPermanente2
+//
+//  Created by Axel Salvatierra on 12/3/20.
+//
+
+import UIKit
+
+class HacerPublicacionViewController: UIViewController {
+    
+    
+}
